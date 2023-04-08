f0d15aa8d2f16e558d7a38e50a17f8010ff54093
Prabh <64233660+Prabh-Saini@users.noreply.github.com>
2023-04-08 21:15:22 +1000

    remove squidconfexample.txt

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ sudo apt install squid
 sudo nano /etc/squid/squid.conf
 wget https://raw.githubusercontent.com/Prabh-Saini/quicksquid/main/squidconfexample.txt
 cp squidconfexample.txt /etc/squid/squid.conf
+rm squidconfexample.txt
 sudo ufw allow 36542
 sudo systemctl restart squid.service
 echo "make sure you allow 36542 for inbound traffic, and 80, 443 for outbound traffic"
